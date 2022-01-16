bbf31ae5b79cd109ec5674e091286cfaea1c515f
Sergey_Ryabtsev <Sergey_Ryabtsev@epam.com>
2022-01-16 22:01:25 +0600

    math operations

--- src/main/java/com/epam/ryabtsev/Test.java
+++ src/main/java/com/epam/ryabtsev/Test.java
@@ -1,6 +1,32 @@
 package com.epam.ryabtsev;
 
 public class Test {
+    public int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public int subtraction(int a, int b) {
+        int result = a - b;
+        return result;
+    }
+
+    public double devision(int a, int b) {
+        double result = a / b;
+
+        return result;
+    }
+
+    public int multiplication(int a, int b) {
+        int result = a * b;
+        return result;
+    }
+
+    public int sqrt(int a) {
+        int result = a * a;
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("init in git");
     }
